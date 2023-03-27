7525177cb2d208d3734d0eb2277683cf32d0f741
gabaraael <gabriel1998.gp45@gmail.com>
2023-03-27 18:10:43 -0300

    Reformulando pagina inicial de boas vindas

--- my-app/src/pages/BemVindo/index.js
+++ my-app/src/pages/BemVindo/index.js
@@ -1,45 +1,77 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { Image, KeyboardAvoidingView, StyleSheet, Text, View } from 'react-native';
+import { Button, Image, KeyboardAvoidingView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 
 export default function BemVindo() {
     return (
-        <View>
-            <KeyboardAvoidingView style={styles.container}>
+        <View style={styles.container}>
+            <View style={styles.containerLogo}>
                 <Image
                     style={styles.tinyLogo}
-                    source={require('../../.././assets/logoBaixaQualidade.png')}
+                    resizeMode="contain"
+                    source={require('../../.././assets/logoTipoSVGMasPNG.png')}
                 />
-            </KeyboardAvoidingView>
+            </View>
             <View >
                 <Text style={[styles.fucshia, styles.title]} >Fuchsia</Text>
             </View>
 
-            <View>
-                <Text>
+            <View style={styles.containerTitle}>
+                <Text style={{ fontWeight: 'bold', alignSelf: 'center' }}>
                     Bem vindo ao
-                    <Text style={[styles.fucshia]}> Fucshia Home Assistant </Text>
-                    ! Deixe me guiá-lo em sua primeira configuração.
+                    <Text style={[styles.fucshia]}> Fucshia Home Assistant</Text>
                 </Text>
+                <View>
+                    <Text style={{ fontWeight: 'bold', alignSelf: 'center' }}>
+                        Deixe me guiá-lo em sua primeira configuração.
+                    </Text>
+                </View>
             </View>
+
+            <View style={styles.containerButton}>
+                <View>
+                    <TouchableOpacity style={styles.button}>
+                        <Text style={styles.textButton}>
+                            Avançar
+                        </Text>
+                    </TouchableOpacity>
+                </View>
+
+                <View>
+                    <TouchableOpacity style={styles.button}>
+                        <Text style={styles.textButton}>
+                            Desisto, não quero minha casa automática
+                        </Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+
+
+
         </View>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex: 32,
+        flex: 1,
         backgroundColor: "#fff",
         alignItems: "center",
         justifyContent: "center",
     },
-    logo: {
-        width: 66,
-        height: 58,
+    containerLogo: {
+        flex: 1,
+    },
+    containerTitle: {
+        flex: 0.5
+        
+    },
+    containerButton: {
+        flex: 2        
     },
     tinyLogo: {
-        width: 100,
-        height: 100
+        width: 230,
+        height: 230
     },
     fucshia: {
         color: "#FF00FF",
@@ -47,5 +79,18 @@ const styles = StyleSheet.create({
     },
     title: {
         fontSize: 32
+    },
+    button: {        
+        borderRadius: 50,
+        paddingVertical: 10,    
+        margin: 5,
+        alignSelf: 'center',
+        width: '80%',       
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#FF00FF'
+    },
+    textButton: {
+        color: 'black'
     }
 });
