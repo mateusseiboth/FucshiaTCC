4df0217b58bedc04b303c288413c4ec5bfcc70d0
mateusseiboth <mateusseiboth@gmail.com>
2023-03-29 14:55:37 -0400

    Modificada página para utilizar componente de template

--- fucshiaHome/src/pages/BemVindo/1.js
+++ fucshiaHome/src/pages/BemVindo/1.js
@@ -1,8 +1,10 @@
 import * as React from 'react';
-import { ActivityIndicator, Image, StyleSheet, Text, View } from 'react-native';
+import { ActivityIndicator, Text, View } from 'react-native';
 import FuchsiaButton from '../../components/FucshiaButton';
 import { useNavigation } from '@react-navigation/native';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
+import SetupTemplate from '../../components/SetupTemplate/index.js';
+import SetupStyle from '../../components/SetupTemplate/style';
 
 export default function Setup1() {
     const navigation = useNavigation();
@@ -19,24 +21,21 @@ export default function Setup1() {
     }
 
     return (
-        <View style={styles.container}>
-            <View style={styles.containerLogo}>
-                <Image
-                    style={styles.tinyLogo}
-                    resizeMode="contain"
-                    source={require('../../.././assets/logoTipoSVGMasPNG.png')}
-                />
-            </View>
-            <View >
-                <Text style={[styles.fucshia, styles.title]} >Fuchsia</Text>
-            </View>
+        <View style={SetupStyle.container}>
+            <SetupTemplate>
 
-            <View style={styles.containerTitle}>
+            </SetupTemplate>
+
+            <View style={SetupStyle.containerTitle}>
                 <Text style={{ fontWeight: 'bold', alignSelf: 'center' }}>
                     Precisamos de acesso a sua localização
-                    <Text style={[styles.fucshia]}> apenas para buscar dispositivos próximos. </Text>
-                    Após a busca os dispositivos irão ser listados abaixo
+                    <Text style={[SetupStyle.fucshia]}> para buscar dispositivos próximos. </Text>
                 </Text>
+                <View>
+                    <Text style={{ fontWeight: 'bold', alignSelf: 'center' }}>
+                       Toque abaixo para permitir
+                    </Text>
+                </View>
             </View>
 
 
@@ -45,47 +44,11 @@ export default function Setup1() {
                 <Text>Buscando</Text>
             </View>) : (<View></View>)}
 
-            <View style={{position: 'absolute', left: 0, right: 0, bottom: 0}} >
-                <View>
-                    <Text style={{ fontWeight: 'bold', alignSelf: 'center' }}>
-                    Toque para iniciar busca</Text>
-                </View>
+            <View style={SetupStyle.containerButton} >
+                
                 <FuchsiaButton text="Buscar" onPress={() => handleAction('wifi')}></FuchsiaButton>
-                <FuchsiaButton text="Avançar" onPress={() => handleRoute('Setup GPIO')}></FuchsiaButton>
+                <FuchsiaButton text="Avançar" onPress={() => handleRoute('Setup2')}></FuchsiaButton>
             </View>
         </View>
     );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#fff",
-        alignItems: "center",
-        justifyContent: "center",
-    },
-    containerLogo: {
-        position: 'absolute', 
-        top: 0
-    },
-    containerTitle: {
-        flex: 0.5
-
-    },
-    containerButton: {
-        flex: 2,
-        width:150,
-        height:50,
-    },
-    tinyLogo: {
-        width: 230,
-        height: 230
-    },
-    fucshia: {
-        color: "#FF00FF",
-        fontWeight: "bold"
-    },
-    title: {
-        fontSize: 32
-    }  
-});
