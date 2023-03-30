913d215307003a9a4d7273495480c6f85cbc1f58
mateusseiboth <mateusseiboth@gmail.com>
2023-03-30 10:34:20 -0400

    Template alterado para atender Issue #11

--- fucshiaHome/src/components/SetupTemplate/index.js
+++ fucshiaHome/src/components/SetupTemplate/index.js
@@ -1,26 +1,33 @@
 import * as React from 'react';
 import { Image, Text, View } from 'react-native';
 import SetupStyle from './style.js';
-
-
-export default function SetupTemplate() {
+//Mateus Seiboth 30/03/2023 - Issue #11 - Template do Setup
+export default function SetupTemplate(props) {
     return (
         <View style={SetupStyle.container}>
-        <View style={SetupStyle.bolinha}></View>
-                <View style={SetupStyle.containerLogo}>
-                    <Image
-                        style={SetupStyle.tinyLogo}
-                        resizeMode="contain"
-                        source={require('../../../assets/logoTipoSVGMasPNG.png')}
-                    />
-                </View>
-            
-               
-            <View style={SetupStyle.container}>
-                <Text style={[SetupStyle.fucshia, SetupStyle.title]} >Fuchsia</Text>
+            <View style={SetupStyle.containerSetup}>
+                <Image
+                    style={SetupStyle.tinyLogo}
+                    resizeMode="contain"
+                    source={require('../../../assets/logoTipoSVGMasPNG.png')}
+                />
+
+                <View style={SetupStyle.seta}></View>
+                <View style={SetupStyle.bolinha}></View>
+                <View style={SetupStyle.seta}></View>
+                <View style={SetupStyle.bolinhaVazia}></View>
+                <View style={SetupStyle.seta}></View>
+                <View style={SetupStyle.bolinhaVazia}></View>
             </View>
-            <View style={[SetupStyle.bolinha, SetupStyle.bolinhaBaixo]}></View>
-            
+
+            <View >
+                <Text style={SetupStyle.title}>
+                    {props.titulo}
+                </Text>
+            </View>
+
+
+
         </View>
     );
 }
