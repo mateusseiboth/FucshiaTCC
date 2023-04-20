55b870441f7eb9f60dd01e619e45daf456420d04
mateusseiboth <mateusseiboth@gmail.com>
2023-04-20 16:01:50 -0400

    Adicionada rota na página de luzes

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,6 +7,7 @@ import FucshiaHome from '../pages/FucshiaHome';
 import Charmander from '../pages/Charmander';
 import Setup2 from '../pages/BemVindo/2.js';
 import Setup3 from '../pages/BemVindo/3.js';
+import Luzes from '../pages/IoT/luzes.js';
 
 const Stack = createNativeStackNavigator();
 
@@ -46,6 +47,12 @@ export default function Routes() {
       <Stack.Screen
         name="Setup3"
         component={Setup3}
+        options={{headerShown: false}}
+      />
+
+      <Stack.Screen
+        name="lamp"
+        component={Luzes}
         options={{headerShown: false}}
       />
     </Stack.Navigator>
