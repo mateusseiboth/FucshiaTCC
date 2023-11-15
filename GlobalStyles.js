6ed562b2f1521535f1fbb329f7b24d77761d6fe3
flaviojrdev <d-garm@outlook.com>
2023-11-14 22:23:13 -0400

    feat: add global styles for color and font

---
+++ GlobalStyles.js
@@ -0,0 +1,27 @@
+export const FontFamily = {
+  montserratBold: "Montserrat-Bold",
+  montserratSemiBold: "Montserrat-SemiBold",
+  montserratExtraBold: "Montserrat-ExtraBold",
+};
+
+export const Color = {
+  colorFuchsia: "#da62d4",
+  colorFuchsiaShadow: "#c24cbd",
+  colorDarkslategray_100: "#434343",
+  colorGray_100: "#808080",
+  colorGray_200: "rgba(0, 0, 0, 0.35)",
+  colorWhite: "#fff",
+  colorOrchid_200: "rgba(218, 98, 212, 0.5)",
+  colorWhitesmoke_100: "#f1f1f1",
+  colorWhitesmoke_200: "#ebebeb",
+  colorWhitesmoke_300: "#eaeaea",
+  colorPlum: "#ecb1ea",
+  colorLightsalmon: "#eb865e",
+  colorCoral: "#db764e",
+  colorMediumturquoise: "#63d5e2",
+  colorLightgray: "#cbcbcb",
+  colorSienna: "#c86239",
+  colorCadetblue: "#41a7b3",
+  colorBlack: "#000",
+  colorSmoke: "#cbcbcb"
+};
